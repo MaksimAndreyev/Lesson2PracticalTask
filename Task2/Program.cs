0b8c1956d90428d6620818ec3999aff6af64155f
maximandreev <maxands@yandex.ru>
2022-08-09 13:46:18 +0300

    Task 2 completed

---
+++ Task2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 100 && number > -100)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+    while (number / 1000 != 0)
+    {
+        number /= 10;
+    }
+    int thirdDigit = number % 10;
+    Console.WriteLine(Math.Abs(thirdDigit));
+}
