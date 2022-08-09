cc1dffc8411fc4fa9f07c585bce6a3fed56eb3ea
maximandreev <maxands@yandex.ru>
2022-08-09 13:51:01 +0300

    Task 3 completed

---
+++ Task3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите номер дня недели:");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number == 6 || number == 7)
+{
+    Console.WriteLine("Выходной");
+}
+else
+{
+    Console.WriteLine("Будний день");
+}
